723af11d388ba7fc417e7c120ae38cb915f53dbb
Camila Doriguello <camila.doriguello@gmail.com>
2023-04-13 21:59:03 -0300

    Upload Projeto

---
+++ src/test/java/primeira/pro/camila/CamilaApplicationTests.java
@@ -0,0 +1,13 @@
+package primeira.pro.camila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
